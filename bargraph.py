26ca4757d53dccca46f6a4131573b44a7496fcf5
mounika <mounika@workcohol.com>
2025-06-11 13:02:27 +0530

    python program

---
+++ bargraph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as mpl
+x=["mouni","sai","raj"]
+y=[50,60,80]
+mpl.bar(x,y)
+mpl.xlabel("students")
+mpl.ylabel("scores")
+mpl.title("student scores")
+mpl.show()
+
---
+++ program.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+x = [2,4,7,9]
+y = [1,6,3,8]
+plt.scatter(x,y) # output: it will show dots
+plt.show()
+
+
